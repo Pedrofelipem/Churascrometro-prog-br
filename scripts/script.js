78d7f66dbe7bcabfcfc23eff2c1c014a8ef11a9b
Pedro Felipe <felipexexeu02@gmail.com>
2021-12-07 21:30:27 -0300

    Script: função calcular

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let inputResultado = document.getElementById('resultado');
+
+function calcular() {
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qtdCarneTotal = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
+    let qtdLitrosCervejaTotal = litrosCervejaPP(duracao) * adultos;
+    let qtdBedidasPP = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+
+    inputResultado.innerHTML = `<p>${qtdCarneTotal / 1000} Kg de carne</p>`
+    inputResultado.innerHTML += `<p>${Math.ceil(qtdLitrosCervejaTotal / 1000)} L de cerveja</P>`
+    inputResultado.innerHTML += `<p>${Math.ceil(qtdBedidasPP / 1000)} L de bebidas</P>`
+
+}
+
+let carnePP = function(duracao) {
+    if (duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    }
+}
+
+let litrosCervejaPP = function(duracao) {
+    if (duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }
+}
+
+let bebidasPP = function(duracao) {
+    if (duracao >= 6) {
+        return 1500;
+    } else {
+        return 1000;
+    }
+}
